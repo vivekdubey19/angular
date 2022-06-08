2affe03e2bd5721e12b9a41ba86bce20dfb66bc5
vivekdubey19 <vireo21@gmail.com>
2022-06-08 12:11:45 +0530

    4 files modified, 2 added

---
+++ src/app/ng-for/ng-for.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for',
+  templateUrl: './ng-for.component.html',
+  styleUrls: ['./ng-for.component.css']
+})
+export class NgForComponent implements OnInit {
+
+  items:any[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
