20e27c4a3b4790bea9d642ebb46968349642939b
vivekdubey19 <vireo21@gmail.com>
2022-06-08 11:39:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-tbpbji
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-tbpbji)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  selectedItem: any;
+
+  selection(ctrl){
+    this.selectedItem = ctrl.value;
+  }
+}
